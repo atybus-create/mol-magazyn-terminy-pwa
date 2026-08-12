70890e26a044e7ea85a2191c246052ed1bb36c85
atybus-create <atybus@estyl.team>
2026-08-12 18:08:46 +0200

    Bump PWA cache after wizard rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mol-magazyn-shell-v2';
+const CACHE='mol-magazyn-shell-v3';
 const PREFIX='mol-magazyn-shell-';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
